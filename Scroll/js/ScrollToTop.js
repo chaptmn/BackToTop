2b5d21eda206fe284531adf826dc2dac9afa883c
Naoya <loutihuji@icloud.com>
2016-04-30 02:46:31 +0900

    fixed

--- Scroll/js/ScrollToTop.js
+++ Scroll/js/ScrollToTop.js
@@ -1,20 +1,38 @@
 
-$(function(){
-  $('#ScrollButton').hide();
+$(function() {
+
+ var ScrollToTop = function(){
+
+  var _scroll_btn = $('#ScrollButton'),
+      _scroll = $(window),
+      _scrollShow,
+      _scrollEvent;
+
+  _scroll_btn.hide();
 
   //topが200px下がったら#ScrollButtonを表示
-  $(window).scroll(function () {
-    if($(this).scrollTop() > 200) {
-        $('#ScrollButton').fadeIn();
-     } else {
-        $('#ScrollButton').fadeOut();
-     }
+  _scrollShow = function(){
+    _scroll.scroll(function(){
+     if( _scroll.scrollTop() > 200) {
+         _scroll_btn.fadeIn();
+       } else {
+         _scroll_btn.fadeOut();
+      }
     });
+   };
+   _scrollShow();
+
   //#ScrollButtonをclickしたらscrollTop:0
-  $('#ScrollButton').click(function () {
-    $('body,html').animate({
-     scrollTop: 0
-    }, 300);
-    return false;
-  });
+  _scrollEvent = function(){
+    _scroll_btn.click(function(){
+      $('body,html').animate({
+       scrollTop: 0
+      }, 300);
+      return false;
+    });
+	};
+  _scrollEvent();
+ };
+
+ ScrollToTop();
 });
